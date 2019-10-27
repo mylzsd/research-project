6b6ccd9ec15ecd527a6310f6a8af64bc8bee89b2
mylzsd <mylzsd@gmail.com>
2019-10-27 17:59:03 +1100

    results

--- mlp.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-FOR %%D in (audiology breast_cancer breast_w cmc dematology ecoli glass hepatitis human_activity iris lymphography) DO (
-	FOR %%N in (50 100) DO (
-		echo "Running python src/test.py -m mlp -d %%D -n %%N > out/mlp/mlp_%%D_%%N.out"
-		python src/test.py -m mlp -d %%D -n %%N > out/mlp/mlp_%%D_%%N.out
-	)
-)
-pause
